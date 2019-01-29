08ff1b33ae4704a77be5a40d5de0efc8e1df4db0
aditya parthasarathy <surayasun59@gmail.com>
2019-01-29 14:04:19 -0800

    javaScript

--- js/formpage.js
+++ js/formpage.js
@@ -9,6 +9,16 @@ var articleText = document.getElementById('article');
 var optionOne = document.getElementById('option-one');
 var optionTwo = document.getElementById('option-two');
 var optionThree = document.getElementById('option-three');
+var table= document.getElementsByTagName('table')[0];
+
+if(localStorage.getItem('website')){
+Element.list= JSON.parse(localStorage.getItem('website'))
+} else{
+  Element.list=[];
+  new Element('title', [''])
+  new Element('header', [''])
+}
+
 
 
 
@@ -36,15 +46,35 @@ Element.store = function() {
 
 function submitHandler(event) {
   event.preventDefault();
+  switch(event.target.type.value){
+    case('img-one'):
+    new Element('img-one', [optionOne.value])
+   break
+    case('img-two'):
+    new Element('img-two', [optionOne.value, optionTwo.value])
+   break
+    case('img-three'):
+    new Element('img-three', [optionOne.value, optionTwo.value, optionThree.value])
+   break
+   case('text'):
+   new Element('text', [optionOne.value])
+   break
+   case('article-left'):
+   new Element('article-left', [optionOne.value, optionTwo.value])
+   break
+   case('article-right'):
+   new Element('article-right', [optionOne.value, optionTwo.value])
+   break
+  }
   
   // reset the storage
-  Element.list = [];
+  // Element.list = [];
 
-  new Element('title', [titleInput.value]);
-  new Element('header', [headerInput.value]);
-  new Element('img-one', [imageOne.value]);
-  new Element('img-three', [imageTwo.value, imageThree.value, imageFour.value])
-  new Element('article-right', [imageFive.value, articleText.value])
+  // new Element('title', [titleInput.value]);
+  // new Element('header', [headerInput.value]);
+  // new Element('img-one', [imageOne.value]);
+  // new Element('img-three', [imageTwo.value, imageThree.value, imageFour.value])
+  // new Element('article-right', [imageFive.value, articleText.value])
 
   Element.store();
 }
@@ -102,4 +132,16 @@ function elementSelect(event){
   }
 }
 document.getElementsByTagName('select')[0].addEventListener('click', elementSelect);
-// elementSelect();
+
+
+function displayList(){
+table.innerHTML='';
+
+  for(var i=0; i< Element.list.length; i++){
+    var firstRow = document.createElement('tr');
+    var tdEl = document.createElement('td');
+    
+  }
+    
+    
+}
